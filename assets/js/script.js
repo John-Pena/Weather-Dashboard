7c5af17b520f8ebc8ebcb187020ed990b0cbd541
John-Pena <jmp1495@gmail.com>
2022-03-18 19:35:40 -0500

    pulled response, but have not gotten city info to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=28a7fce4f20896b97ae391942a7e9c8d
+
+var button = document.querySelector('.btn');
+var cityName = document.querySelector('.value');
+
+var weatherUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&appid=28a7fce4f20896b97ae391942a7e9c8d"
+
+var getWeather = function() {
+    fetch(weatherUrl).then(function(response) {
+        console.log(response);
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+            });
+        };
+    });
+}
+
+
+button.addEventListener('click', getWeather());

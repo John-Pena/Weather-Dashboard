fb7daac4a964f50ff89c4e70fd9c2481673e255e
John-Pena <jmp1495@gmail.com>
2022-03-18 21:11:53 -0500

    pulled city temp and other data using hardcode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=28a7fce4f20896b97ae391942a7e9c8d
 
-var button = document.querySelector('.btn');
-var cityName = document.querySelector('.value');
 
-var weatherUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&appid=28a7fce4f20896b97ae391942a7e9c8d"
+// var cityName = document.querySelector('.value');
+// var button = document.querySelector('.btn')
+var APIKey = "28a7fce4f20896b97ae391942a7e9c8d";
 
-var getWeather = function() {
-    fetch(weatherUrl).then(function(response) {
+// var cityUrl = "api.openweathermap.org/data/2.5/forecast?q=" + cityName.value + "&appid="+ APIKey + "&units=imperial";
+var cityUrl = "https://api.openweathermap.org/data/2.5/forecast?q=Austin&limit=5&appid="+ APIKey + "&units=imperial";
+
+var getCityInfo = function() {
+    fetch(cityUrl).then(function(response) {
         console.log(response);
         if (response.ok) {
             response.json().then(function(data) {
@@ -14,7 +16,6 @@ var getWeather = function() {
             });
         };
     });
-}
+};
 
-
-button.addEventListener('click', getWeather());
+getCityInfo();

70b782b00331d842631f21ee4fcad9936b8936c1
John-Pena <jmp1495@gmail.com>
2022-03-19 11:45:45 -0500

    got day of weather to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,16 @@
-// var cityName = document.querySelector('.value');
-// console.log(cityName.value);
+var cityName = document.querySelector('.value');
+console.log(cityName.text);
 // var button = document.querySelector('.btn')
 
 // fecthes weather formation from API
 var getCityInfo = function() {
+
     var APIKey = "28a7fce4f20896b97ae391942a7e9c8d";
     var cityUrl = "http://api.openweathermap.org/geo/1.0/direct?q=SanAntonio&limit=5&appid=" + APIKey;
 
     fetch(cityUrl).then(function(response) {
-        console.log(response);
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data);
                 getCityWeather(data);
             });
         };
@@ -19,7 +18,6 @@ var getCityInfo = function() {
 };
 
 var getCityWeather = function(data) {
-    console.log('hello');
 
     // pulls lat from GeoAPI
     var lat = data[0].lat;
@@ -46,14 +44,27 @@ var getCityWeather = function(data) {
 
 var displayWeather = function(weather) {
 
+    // pulls sky description ftom array API
+    var skyDesc = document.querySelector('.sky-description');
+    skyDesc.textContent = weather.list[0].weather[0].description;
+
     // pulls temperature info from array
     var tempEl = document.createElement('span');
     tempEl.textContent = weather.list[0].main.temp;
     console.log(tempEl);
     
     // append temperature to container with class of display
-    var dayOfTemp = document.querySelector('.day-of-temp')
+    var dayOfTemp = document.querySelector('.day-of-temp');
     dayOfTemp.appendChild(tempEl);
+
+    // Getting Humidity info from array
+    var humidityEl = document.createElement('span');
+    humidityEl.textContent = weather.list[0].main.humidity;
+    console.log(humidityEl);
+
+    // append humidity to container with class of display
+    var dayOfHumidity = document.querySelector('.day-of-humidity');
+    dayOfHumidity.appendChild(humidityEl);
 
     // Getting wind speed from array
     var windEl = document.createElement('span');
@@ -63,8 +74,6 @@ var displayWeather = function(weather) {
     // append wind speed to container with class of display
     var dayOfWind = document.querySelector('.day-of-wind');
     dayOfWind.appendChild(windEl);
-
-    // Getting 
 };
 
 getCityInfo();

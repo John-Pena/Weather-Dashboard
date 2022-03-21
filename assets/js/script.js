3b49f76a2abaa6fb9343ae45ea5cb38db5ea45e6
John-Pena <jmp1495@gmail.com>
2022-03-20 20:40:38 -0500

    refactired code to remove icons in city name, refactored html to allow weather to display cleanly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
-var citySearch = document.location.search;
-console.log(citySearch);
-
+var citySearch = document.querySelector('#city-input');
+var userInput = document.querySelector('#value');
 var forecast = document.querySelector('.day-of-forecast');
 
-// fecthes lat and lon from city name search
-var getCityInfo = function() {
+// listens for submit on form
+function formHandler(event) {
+    event.preventDefault();
 
-    var cityName = citySearch.split('=')[1];
+    var cityName = citySearch.value.trim();
+
+    if (cityName) {
+        getCityInfo(cityName);
+    }
+    else {
+        alert('Error: City name does not exist');
+    };
+
+};
+
+// fecthes lat and lon from city name search
+var getCityInfo = function (cityName) {
 
     var displayCityName = document.querySelector('.city-name');
     displayCityName.textContent = cityName + ' ';
@@ -14,15 +26,15 @@ var getCityInfo = function() {
     var date = document.createElement('span');
     date.textContent = moment().format('LL');
     displayCityName.appendChild(date);
-    
+
 
     var APIKey = "28a7fce4f20896b97ae391942a7e9c8d";
-    var cityUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=" + APIKey;
+    var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
 
-    fetch(cityUrl).then(function(response) {
+    fetch(cityUrl).then(function (response) {
         if (response.ok) {
-            response.json().then(function(data) {
-                getCityWeather(data);
+            response.json().then(function (data) {
+                getCityWeather(data.coord);
                 console.log(data);
             });
         };
@@ -30,92 +42,61 @@ var getCityInfo = function() {
 };
 
 // gets weather from city using lat and lon
-var getCityWeather = function(data) {
-
-    // pulls lat from GeoAPI
-    var lat = data[0].lat;
-    console.log(lat);
-
-    // pulls lon from GeoAPI
-    var lon = data[0].lon;
-    console.log(lon);
+var getCityWeather = function (coord) {
 
     // city weather API call
     var APIKey = "28a7fce4f20896b97ae391942a7e9c8d";
-    var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=" + APIKey;
+    var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coord.lat + "&lon=" + coord.lon + "&exclude=minutely,hourly&units=imperial&appid=" + APIKey;
 
-    fetch(weatherUrl).then(function(response) {
+    fetch(weatherUrl).then(function (response) {
         console.log(response);
-        if(response.ok) {
-            response.json().then(function(data) {
+        if (response.ok) {
+            response.json().then(function (data) {
                 console.log(data);
-                displayDayOfWeather(data);
+                // displayDayOfWeather(data.current);
+                fiveDayForecast(data.daily);
+                var current = data.current;
+                // pulls sky description ftom array API
+                var skyDesc = document.querySelector('.sky-description');
+                skyDesc.textContent = current.weather[0].description;
+
+                // displays weather icon
+                var icon = current.weather[0].icon;
+                var showIcon = document.createElement('img');
+                showIcon.src = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+                skyDesc.appendChild(showIcon);
+
+                // pulls temperature info from array
+                var tempEl = document.querySelector('#temp');
+                tempEl.textContent = current.temp;
+
+                // Getting Humidity info from array
+                var humidityEl = document.querySelector('#humidity');
+                humidityEl.textContent = current.humidity;
+
+                // Getting wind speed from array
+                var windEl = document.querySelector('#wind');
+                windEl.textContent = current.wind_speed;
+
+                // pulls uv Index from current array
+                var uvIndex = document.querySelector('#uv');
+                uvIndex.textContent = current.uvi;
+
+                // if statement for uv index color dictating low uv to extreme uv
+                // if (uvIndex < 3) {
+                //     forecast.classList.remove('')
+                //     forecast.classList.add('low-uv-index');
+
+                // }
             });
         };
     });
 };
 
-// displays day of weather
-var displayDayOfWeather = function(weather) {
+var fiveDayForecast = function () {
+    // first day forecast
 
-    // pulls sky description ftom array API
-    var skyDesc = document.querySelector('.sky-description');
-    skyDesc.textContent = weather.current.weather[0].description;
-    // displays weather icon
-    var icon = weather.current.weather[0].icon;
-    var showIcon = document.createElement('img');
-    showIcon.src = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
-    skyDesc.appendChild(showIcon);
-
-    // pulls temperature info from array
-    var tempEl = document.createElement('span');
-    tempEl.textContent = weather.current.temp;
-    // append temperature to container with class of display
-    var dayOfTemp = document.querySelector('.day-of-temp');
-    dayOfTemp.appendChild(tempEl);
-
-    // Getting Humidity info from array
-    var humidityEl = document.createElement('span');
-    humidityEl.textContent = weather.current.humidity;
-    // append humidity to container with class of display
-    var dayOfHumidity = document.querySelector('.day-of-humidity');
-    dayOfHumidity.appendChild(humidityEl);
-
-    // Getting wind speed from array
-    var windEl = document.createElement('span');
-    windEl.textContent = weather.current.wind_speed;
-    // append wind speed to container with class of display
-    var dayOfWind = document.querySelector('.day-of-wind');
-    dayOfWind.appendChild(windEl);
-
-    // pulls uv Index from current array
-    var uvIndex = document.createElement('span');
-    uvIndex.textContent = weather.current.uvi;
-    // append uv Index to dom
-    var dayOfIndex = document.querySelector('.day-of-UV');
-    dayOfIndex.appendChild(uvIndex);
-
-    // if statement for uv index color dictating low uv to extreme uv
-    if (uvIndex >= 11) {
-        // adds purple to background
-        forecast.classList.add('.extreme-uv-index');
-    }
-    else if (uvIndex >= 8 && uvIndex < 10) {
-        // adds red to background
-        forecast.classList.add('very-high-uv-index');
-    }
-    else if (uvIndex >= 6 && uvIndex < 7) {
-        // adds orange to background
-        forecast.classList.add('high-uv-index');
-    }
-    else if (uvIndex >= 3 && uvIndex < 5) {
-        // adds yellow to background
-        forecast.classList.add('moderate-uv-index');
-    }
-    else {
-        // adds green to background
-        forecast.classList.add('low-uv-index');
-    };
 };
 
-getCityInfo();
+userInput.addEventListener('submit', formHandler);
+// getCityInfo();

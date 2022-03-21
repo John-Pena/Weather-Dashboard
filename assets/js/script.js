15d9a6d9450fb567e77e3eab67ad25c0606f0fb1
John-Pena <jmp1495@gmail.com>
2022-03-20 22:24:39 -0500

    DRY coded forecast, could not firgure out how to loop it to the dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -94,15 +94,156 @@ var getCityWeather = function (coord) {
                     uvIndex.classList.add('tag extreme-uv-index');
                 };
 
+                firstDayForecast(data.daily)
+                secondDayForecast(data.daily)
+                thirdDayForecast(data.daily)
+                fourthDayForecast(data.daily)
                 fiveDayForecast(data.daily)
             });
         };
     });
 };
 
-var fiveDayForecast = function (data) {
+var firstDayForecast = function (daily){
+
+    var skyDesc = document.querySelector('.sky-description-1');
+    skyDesc.textContent = daily[0].weather[0].description;
+
+    // displays weather icon
+    var icon = daily[0].weather[0].icon;
+    var showIcon = document.createElement('img');
+    showIcon.src = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+    skyDesc.appendChild(showIcon);
+
+    // pulls temperature info from array
+    var tempEl = document.querySelector('#temp-1');
+    tempEl.textContent = daily[0].temp.day;
+
+    // Getting Humidity info from array
+    var humidityEl = document.querySelector('#humidity-1');
+    humidityEl.textContent = daily[0].humidity;
+
+    // Getting wind speed from array
+    var windEl = document.querySelector('#wind-1');
+    windEl.textContent = daily[0].wind_speed;
+
+    // pulls uv Index from current array
+    var uvIndex = document.querySelector('#uv-1');
+    uvIndex.textContent = daily[0].uvi;
+};
+
+var secondDayForecast = function (daily) {
+
+    var skyDesc = document.querySelector('.sky-description-2');
+    skyDesc.textContent = daily[1].weather[0].description;
+
+    // displays weather icon
+    var icon = daily[1].weather[0].icon;
+    var showIcon = document.createElement('img');
+    showIcon.src = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+    skyDesc.appendChild(showIcon);
+
+    // pulls temperature info from array
+    var tempEl = document.querySelector('#temp-2');
+    tempEl.textContent = daily[1].temp.day;
+
+    // Getting Humidity info from array
+    var humidityEl = document.querySelector('#humidity-2');
+    humidityEl.textContent = daily[1].humidity;
+
+    // Getting wind speed from array
+    var windEl = document.querySelector('#wind-2');
+    windEl.textContent = daily[1].wind_speed;
+
+    // pulls uv Index from current array
+    var uvIndex = document.querySelector('#uv-2');
+    uvIndex.textContent = daily[1].uvi;
+};
+
+var thirdDayForecast = function (daily) {
+
+    var skyDesc = document.querySelector('.sky-description-3');
+    skyDesc.textContent = daily[2].weather[0].description;
     
+    // displays weather icon
+    var icon = daily[2].weather[0].icon;
+    var showIcon = document.createElement('img');
+    showIcon.src = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+    skyDesc.appendChild(showIcon);
+
+    // pulls temperature info from array
+    var tempEl = document.querySelector('#temp-3');
+    tempEl.textContent = daily[2].temp.day;
+
+    // Getting Humidity info from array
+    var humidityEl = document.querySelector('#humidity-3');
+    humidityEl.textContent = daily[2].humidity;
+
+    // Getting wind speed from array
+    var windEl = document.querySelector('#wind-3');
+    windEl.textContent = daily[2].wind_speed;
+
+    // pulls uv Index from current array
+    var uvIndex = document.querySelector('#uv-3');
+    uvIndex.textContent = daily[2].uvi;3
+};
+
+var fourthDayForecast = function (daily) {
+
+    var skyDesc = document.querySelector('.sky-description-4');
+    skyDesc.textContent = daily[3].weather[0].description;
+    
+    // displays weather icon
+    var icon = daily[3].weather[0].icon;
+    var showIcon = document.createElement('img');
+    showIcon.src = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+    skyDesc.appendChild(showIcon);
+
+    // pulls temperature info from array
+    var tempEl = document.querySelector('#temp-4');
+    tempEl.textContent = daily[3].temp.day;
+
+    // Getting Humidity info from array
+    var humidityEl = document.querySelector('#humidity-4');
+    humidityEl.textContent = daily[3].humidity;
+
+    // Getting wind speed from array
+    var windEl = document.querySelector('#wind-4');
+    windEl.textContent = daily[3].wind_speed;
+
+    // pulls uv Index from current array
+    var uvIndex = document.querySelector('#uv-4');
+    uvIndex.textContent = daily[3].uvi;4
+};
+
+var fiveDayForecast = function (daily) {
+
+    var skyDesc = document.querySelector('.sky-description-5');
+    skyDesc.textContent = daily[4].weather[0].description;
+
+    // displays weather icon
+    var icon = daily[4].weather[0].icon;
+    var showIcon = document.createElement('img');
+    showIcon.src = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+    skyDesc.appendChild(showIcon);
+
+    // pulls temperature info from array
+    var tempEl = document.querySelector('#temp-5');
+    tempEl.textContent = daily[4].temp.day;
+
+    // Getting Humidity info from array
+    var humidityEl = document.querySelector('#humidity-5');
+    humidityEl.textContent = daily[4].humidity;
+
+    // Getting wind speed from array
+    var windEl = document.querySelector('#wind-5');
+    windEl.textContent = daily[4].wind_speed;
+
+    // pulls uv Index from current array
+    var uvIndex = document.querySelector('#uv-5');
+    uvIndex.textContent = daily[4].uvi;5
+
+
 };
 
 userInput.addEventListener('submit', formHandler);
-// getCityInfo();
